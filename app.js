19f476170989c31e1c6ca0b4fb3b9b86c94f107d
Iryna <iryna.kolhanova@gmail.com>
2024-01-20 17:17:04 -0500

    fix Lesson 13: Authentication with Passport

--- app.js
+++ app.js
@@ -40,15 +40,6 @@ if (app.get("env") === "production") {
 
 app.use(session(sessionParms));
 
-passportInit();
-app.use(passport.initialize());
-app.use(passport.session());
-
-// this code must come after the app.use that sets up sessions, because flash depends on sessions
-app.use(require("connect-flash")());
-
-app.use(require("./middleware/storeLocals"));
-
 // after cookie_parser and any body parsers but before any of the routes.
 app.use(cookieParser(process.env.SESSION_SECRET));
 app.use(express.urlencoded({ extended: false }));
@@ -60,16 +51,19 @@ if (app.get("env") === "production") {
 }
 const csrf_options = {
   protected_operations: ["PATCH", "PUT", "POST"],
-  protected_content_types: ["application/json"],
+  protected_content_types: ["application/json", "application/x-www-form-urlencoded"], // protect headers from
   development_mode: csrf_development_mode,
 };
 app.use(csrf(csrf_options));
 
+passportInit();
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
-app.use("/sessions", require("./routes/sessionRoutes"));
+// this code must come after the app.use that sets up sessions, because flash depends on sessions
+app.use(require("connect-flash")());
+
+app.use(require("./middleware/storeLocals"));
 
 // These lines should be added before any of the lines that govern routes, such as the app.get and app.post statements:
 // app.use(
@@ -81,7 +75,14 @@ app.use("/sessions", require("./routes/sessionRoutes"));
 // );
 
 // secret word handling
+
+// routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.use("/sessions", require("./routes/sessionRoutes"));
 app.use("/secretWord", auth, secretWordRouter);
+
 
 app.use((req, res) => {
   res.status(404).send(`That page (${req.url}) was not found.`);
